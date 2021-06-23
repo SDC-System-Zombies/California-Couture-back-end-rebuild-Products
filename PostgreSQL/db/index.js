799da6e7b3f56d38a32cd4aa7630876e63a6f340
Vy Tran <vtran1022@Gmail.com>
2021-06-23 10:50:44 -0700

    added db folder to contain token and connection to pg;

---
+++ PostgreSQL/db/index.js
@@ -0,0 +1,15 @@
+const { Pool } = require('pg');
+const token = require('token.js');
+
+const pool = new Pool({
+  user: 'vytran',
+  host: 'localhost',
+  database: 'atelier',
+  password: token,
+  port:5432,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
+
+module.exports = pool;
