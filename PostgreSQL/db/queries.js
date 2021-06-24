b2c048a33cb6a479532646378a44faf501237fb5
Vy Tran <vtran1022@Gmail.com>
2021-06-24 15:34:53 -0700

    wrote queries

---
+++ PostgreSQL/db/queries.js
@@ -0,0 +1,41 @@
+const pool = require('index.js');
+
+const fetchAllProducts = () => {
+    const queryStr = 'SELECT * FROM products LIMIT 5';
+    pool.query(queryStr)
+    .then((data) => data)
+    .catch((err) => `Query fetching products failed: ${err}`);
+};
+
+const fetchProduct = (params) => {
+  const getProductQuery = 'SELECT * FROM products WHERE productId = $1';
+  const getFeatureQuery = 'SELECT feature, value FROM features WHERE productId = $1';
+
+  Promise.all([
+    pool.query(getProductQuery, params),
+    pool.query(getFeatureQuery, params)
+  ])
+  .then((data) => data)
+  .catch((err) => `Query fetching product failed: ${err}`);
+};
+
+const fetchStyles = (params) => {
+  const queryStr = 'SELECT styleId, name, sale_price, original_price, default_style FROM styles WHERE productId = $1';
+  pool.query(queryStr, params);
+  .then((data) => data)
+  .catch((err) => `Query fetching styles failed: ${err}`);
+};
+
+const fetchSkus = (params) => {
+  const queryStr = 'SELECT size, quantity FROM styles WHERE styleId = $1';
+  pool.query(queryStr, params);
+  .then((data) => data)
+  .catch((err) => `Query fetching skus failed: ${err}`);
+};
+
+const fetchRelated = (params) => {
+  const queryStr = 'SELECT relatedId FROM related WHERE productId = $1';
+  pool.query(queryStr, params);
+  .then((data) => data)
+  .catch((err) => `Query fetching related products failed: ${err}`);
+};
