311f549513f0f8a479bf78e755e7ad4c3702a24a
Vy Tran <vtran1022@Gmail.com>
2021-06-23 18:56:14 -0700

    update url

--- MongoDB/db/index.js
+++ MongoDB/db/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 mongoose.connect('mongodb://localhost:27017/atelier', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const productSchema = new Schema({
