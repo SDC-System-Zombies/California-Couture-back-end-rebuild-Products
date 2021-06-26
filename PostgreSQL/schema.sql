e8fb09beb9676fccf79ce018f83c8cbe7c1f6abf
Vy Tran <vtran1022@Gmail.com>
2021-06-26 14:52:09 -0700

    added index statements

--- PostgreSQL/schema.sql
+++ PostgreSQL/schema.sql
@@ -111,3 +111,11 @@ COPY skus
 FROM '/Users/vytran/Downloads/skus.csv'
 DELIMITER ','
 CSV HEADER;
+
+-- create indexes
+
+CREATE INDEX style_prod_index ON styles(product_id);
+CREATE INDEX photo_style_index ON photos(style_id);
+CREATE INDEX sku_style_index ON skus(style_id);
+CREATE INDEX features_prod_index ON features(product_id);
+CREATE INDEX related_prod_index ON related(product_id);
