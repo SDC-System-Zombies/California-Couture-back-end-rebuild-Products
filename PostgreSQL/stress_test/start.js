7c91d8b2220c1bbf1ad6ff21470d30def24b00ac
Vy Tran <vtran1022@Gmail.com>
2021-06-30 11:20:32 -0700

    updating some tests

--- PostgreSQL/stress_test/start.js
+++ PostgreSQL/stress_test/start.js
@@ -9,11 +9,11 @@ export let options = {
   scenarios: {
     open_model: {
       executor: 'constant-arrival-rate',
-      rate: 1000,
+      rate: 5000,
       timeUnit: '1s',
       duration: '30s',
-      preAllocatedVUs: 1,
-      maxVUs: 1
+      preAllocatedVUs: 110,
+      maxVUs: 110
     },
   }
 };
